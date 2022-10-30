1938a36749023bef949fa183d1db9fc8a666435c
EnzoLuidge <enzolpsb@al.insper.edu.br>
2022-10-30 02:09:19 -0300

    end

--- script.js
+++ script.js
@@ -27,5 +27,69 @@ function handleSymbol(symbol){
             buffer = runningTotal;
             runningTotal = 0;
             break;
+        case '←':
+            console.log('oi')
+            if(buffer.length === 1){
+                buffer = '0';
+            }
+            else{
+                buffer = buffer.substring(0,buffer.length-1);
+            }
+            break;
+        case '+':
+        
+        case "−":
+        
+        case "×":
+
+        case "÷":
+            handleMath(symbol);
+            break;
     }
 }
+function handleMath(symbol){
+    if(buffer==='0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal===0){
+        runningTotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
+    previousOperator=symbol;
+    buffer='0';
+
+}
+function flushOperation(intBuffer){
+    if(previousOperator==='+'){
+        runningTotal += intBuffer;
+    }
+    else if(previousOperator=== '−'){
+        runningTotal -=intBuffer;
+    }
+    else if(previousOperator==="×"){
+        runningTotal *=intBuffer;
+    }
+    else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer==='0'){
+        buffer=numberString;
+    }
+    else{
+        buffer +=numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener('click',function(event){
+        buttonClick(event.target.innerText);
+    })
+}
+
+init();
